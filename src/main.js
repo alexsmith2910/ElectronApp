c812b13361047c57cb729d733b9232af52044667
alexsmith2910 <40245164+alexsmith2910@users.noreply.github.com>
2020-11-02 22:50:43 +0000

    Landing Page Updated

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    minWidth: 1000,
+    minHeight: 800,
+    webPreferences: {
+        nodeIntergration: true
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'public/html/index.html'));
+  mainWindow.removeMenu()
+
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
